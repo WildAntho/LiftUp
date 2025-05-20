92d347647e70901912a0b2adc5857e61b04d6ddc
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2025-05-20 12:40:54 +0200

    reset count on muscle group entity

--- backend/src/migrations/1747733066681-Musclegroup.ts
+++ backend/src/migrations/1747733066681-Musclegroup.ts
@@ -2,6 +2,9 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Musclegroup1747733066681 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `TRUNCATE TABLE muscle_group RESTART IDENTITY CASCADE`
+    );
     await queryRunner.query(`INSERT INTO muscle_group ("key", "label") VALUES
             ('back', 'Dos'),
             ('pecs', 'Pectoraux'),
