3087bdc9d29e1e97b8268181e4bd804200b3edd4
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2025-05-26 16:29:42 +0200

    remove progress from userwithoutpassword type

--- frontend/src/services/zustand/userStore.ts
+++ frontend/src/services/zustand/userStore.ts
@@ -4,7 +4,7 @@ import { persist } from "zustand/middleware";
 
 export type UserWithoutPassword = Omit<
   User,
-  "password" | "coach" | "students" | "crew" | "notificationPreferences"
+  "password" | "coach" | "students" | "crew" | "notificationPreferences" | "progress"
 >;
 
 type UserState = {
