658ac13ace9b57abfbb53ff556568ef02f61fec4
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2025-07-29 19:51:39 +0200

    add filter on market place program + skeleton

--- frontend/src/queries/Program/GetProgramsMarket.ts
+++ frontend/src/queries/Program/GetProgramsMarket.ts
@@ -1,8 +1,16 @@
 import { gql } from "@apollo/client";
 
 export const GET_PROGRAM_MARKETPLACE = gql`
-  query GetProgramsMarketPlace {
-    getProgramsMarketPlace {
+  query GetProgramsMarketPlace(
+    $price: [Float!]
+    $categorie: String
+    $level: ProgramLevel
+  ) {
+    getProgramsMarketPlace(
+      price: $price
+      categorie: $categorie
+      level: $level
+    ) {
       id
       title
       duration
